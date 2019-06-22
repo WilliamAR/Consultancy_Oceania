7a14cd2f101c1df3f520c9cc1a737ae7fa762005
WilliamAR <waguilarr@unal.edu.co>
2019-06-22 16:53:17 -0500

    Eficiencia en calculo de medias, correccion de graficos

--- Function.R
+++ Function.R
@@ -1,71 +1,49 @@
-#Abrir archivos####
-#68 lineas inservibles 
+#Install packages####
 install.packages("lubridate")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("ggplotAssist") #Sugerido
+#Library
 library(lubridate)
+library(dplyr)
+library(ggplot2)
+library(ggplotAssist)
+#Open file####
+#68 lineas inservibles 
 datos <- read.table(file = "AirSea_ST_0001.asc", skip = 68,header = T)
-#Arreglando la base####
+#Fix the base####
 datos <- datos[datos$SSH != -99,c("YYYYMM","SSH")]
 datos$YYYYMM <- ymd(datos$YYYYMM,truncated = 2)
 datos$Month <- month(datos$YYYYMM)
 datos$Year <- year(datos$YYYYMM)
 datos <- datos[,c("Year","Month","SSH")]
-boxplot(datos$SSH ~ datos$Year, las = 1, main = "Sea surface height", xlab = "Year",
-        ylab = "Height", ylim = c(0,max(datos$SSH)))
-Mean_year <- function(Year,SSH){
-  Y <- c()
-  MA <- c()
-  suma <- SSH[1]
-  meses <- 1
-  k <- 1
-  for(i in 2:length(Year)){
-    
-    if(Year[i-1]==Year[i]){
-      suma <- suma + SSH[i]
-      meses <- meses + 1
-      if(i == length(Year)){
-        Y[k] <- Year[i]
-        MA[k] <- suma/meses
-      }
-    }else{
-      Y[k] <- Year[i-1]
-      MA[k] <- suma/meses
-      suma <- SSH[i]
-      meses <- 1
-      k <- k + 1
-    }
-  }
-  return(data.frame(SSH= MA,Year=Y))
-}
-MY <- Mean_year(datos$Year,datos$SSH)
-lines(MY,col = "red", type = 'b', pch=19)
-
-boxplot(datos$SSH ~ datos$Month, las = 1, main = "Sea surface height", xlab = "Month",
-        ylab = "Height", ylim = c(0,max(datos$SSH)))
-Mean_month <- function(Month,SSH){
-  MM <- rep(NA,times = 12)
-  for(i in 1:12){
-    MM[i] <- mean(datos[datos$Month==i,"SSH"])
-  }
-  return(as.data.frame(MM))
-}
-MM <- Mean_month(datos$Month,datos$SSH)
-lines(x,col = "red", type = 'b', pch=19)
-
-install.packages("ggplot2")
-install.packages("ggplotAssist")
-library(ggplot2)
-library(ggplotAssist)
-datos <- datos
 datos$Month <- factor(datos$Month,
-                           labels = c("Ene","Feb","Mar","Abr","May", "Jun", 
-                                      "Jul", "Aug", "Sep", "Oct","Nov", "Dic"))
+                      labels = c("Ene","Feb","Mar","Abr","May", "Jun", 
+                                 "Jul", "Aug", "Sep", "Oct","Nov", "Dic"))
+#Means Year and Month####
+MY <- datos %>% group_by(Year) %>% summarise(medias = mean(SSH))
+MM <- datos %>% group_by(Month) %>% summarise(medias = mean(SSH))
 
-BPY <- ggplot(datos, aes(x=factor(Year),y=SSH)) +
-  geom_boxplot(fill = "#aee7e8", outlier.color = "#24009c") +
-  scale_y_continuous(name = "Height") + scale_x_discrete(name = "Year") +
-  theme_light() + labs(tag = "Sea surface height") +
+ggplot(data = MY, aes(x=Year, y=medias)) + 
+  geom_boxplot(data = datos,aes(x=Year,y=SSH,group=Year), 
+               fill = "#aee7e8", outlier.color = "#24009c") +
+  scale_y_continuous(name = "Height") + xlab("Year") + 
+  labs(tag = "Sea surface height") + 
   theme(plot.tag = element_text(lineheight = 2,face = "bold",size = 20),
-        plot.tag.position = "top")  + geom_point(data = MY, color = "#c72c41")+
-  geom_line(data = MY,x=as.numeric(Year), color = "#c72c41")
-BPY
+        plot.tag.position = "top")  +
+  geom_point(color = "#c72c41") + geom_line(color = "#c72c41")
+
+
+ggplot(data = MM, aes(x=Month, y=medias)) + 
+  geom_boxplot(data = datos,aes(x=Month,y=SSH,group=Month), 
+               fill = "#aee7e8", outlier.color = "#24009c") + 
+  scale_y_continuous(name = "Height") + 
+  scale_x_continuous(name = "Month", breaks = 1:12 , labels = 
+                       c("Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep",
+                         "Oct","Nov","Dic")) +
+  labs(tag = "Sea surface height") + 
+  theme(plot.tag = element_text(lineheight = 2,face = "bold",size = 20),
+        plot.tag.position = "top")  +
+  geom_point(color = "#c72c41")  + geom_line(color = "#c72c41")
+
 

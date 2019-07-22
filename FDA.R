77cac68c26f1c64d352b5b8b8f1d0a8b2e5552fe
WilliamAR <waguilarr@unal.edu.co>
2019-07-22 10:33:11 -0500

    correccion de datos$SSH por datos lineas 18,20,22,41 y error linea 40 de dataframe

--- FDA.R
+++ FDA.R
@@ -15,11 +15,11 @@ datos <- read.table(file = "AirSea_ST_0001.asc", skip = 68,header = T)
 datos <- datos[datos$SSH != -99,c("SSH")]
 #Data analysis####
 Quant <- data.frame(Probs=seq(0,1,0.0001),
-                    Cuantil_P=quantile(datos$SSH,probs = seq(0,1,0.0001)))
+                    Cuantil_P=quantile(datos,probs = seq(0,1,0.0001)))
 
-Ajuste <- fitDist(y = SSH, type = "realplus", data = datos)
+Ajuste <- fitDist(y = datos, type = "realplus")
 
-histDist(datos$SSH, family=as.name(Ajuste$family[1]), col.hist = "white", 
+histDist(datos, family=as.name(Ajuste$family[1]), col.hist = "white", 
          col.main = "Black", line.col = "blue",border.hist = "black",
          main= paste("Family:",Ajuste$family[2]), xlab="Datos",ylab="Densidad",
          col.axis = "black", col.lab = "black")
@@ -37,8 +37,8 @@ d_0.95 <- round(Quant$Cuantil_P[9501],2)
 d_0.99 <- round(Quant$Cuantil_P[9901],2)
 ggplot(data = Quant) + 
   geom_line(aes(x=Cuantil_P,y=Probs), color = "#FF9770", size=1) +
-  geom_line(aes(x=Cuantil_T,y=Probs), color = "#70D6FF", size=1) + 
-  scale_x_continuous(name = "X",breaks = seq(0,ceiling(max(datos$SSH)),5)) +
+  geom_line(aes(x=Cuantil_P,y=Probs), color = "#70D6FF", size=1) + 
+  scale_x_continuous(name = "X",breaks = seq(0,ceiling(max(datos)),5)) +
   scale_y_continuous(name = "Probability") +
   labs(tag = paste("Data VS",Ajuste$family[2])) +
   theme(plot.tag = element_text(lineheight = 2,face = "bold",size = 15),
